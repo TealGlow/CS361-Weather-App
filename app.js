96b1e6a3ef0fcc3921ffac1772d5e68d36458d85
TealGlow <alyssacomstock95@gmail.com>
2021-11-28 19:13:39 -0800

    Fixed a bug when entering invalid zip code attempting to set headers

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require("body-parser");
-//const path = require('path');
 const axios = require('axios').default;
 
 const app = express();
@@ -26,67 +25,61 @@ app.get("/",(req,res)=>{
 
 
 app.post("/", (req, res)=>{
-  // form validation
+
   var user_input = req.body;
 
   err = "Error in validating location, please try again";
 
-  /*
-      VALIDATE LOCATION AND INPUT HERE
-
-  */
-
+  // user input is validated here
   if(user_input.userLoc.length<5 || parseInt(user_input.userLoc) != user_input.userLoc){
     // if the code is less than 5, we are going to send the user an error
     res.render("pages/index",{message:"Please enter a valid zip code."});
   }else{
-    // validate zip and req wiki web scraper
-    const user_input_zip = user_input.userLoc;
-    const validate_zip = axios.get(ZIP_CODE_VALIDATE_URL+user_input_zip)
-    .then((response)=>{
-      // We got a response from zip service. request the wikipedia web scraper.
-
-      if(response.data.valid_zip != true){
-        // if the zip code is not valid, we are not going to request the wikipedia
-        res.render("pages/index", {message:"Invalid zip code."});
-      }
-
-      const wiki_weather_api_request = axios.get(Wiki_API_URL+"?zip="+user_input_zip)
+      // validate zip and req wiki web scraper
+      const user_input_zip = user_input.userLoc;
+      const validate_zip = axios.get(ZIP_CODE_VALIDATE_URL+user_input_zip)
       .then((response)=>{
-        // got the wikipedia data correctly.
+        // We got a response from zip service. request the wikipedia web scraper.
+        if(response.data.valid_zip != true){
+          // if the zip code is not valid, we are not going to request the wikipedia
+          res.render("pages/index", {message:"Invalid zip code."});
+        }else{
+          const wiki_weather_api_request = axios.get(Wiki_API_URL+"?zip="+user_input_zip)
+          .then((response)=>{
+            // got the wikipedia data correctly.
 
-        // data we got back from the response_data
-        console.log("success,", response);
+            // data we got back from the response_data
+            console.log("successfully received data");
 
-        response_data = {
-          weather_current:response.data.weather.current,
-          five_day:response.data.weather.five_day,
-          location_info:response.data.location_info,
-          wiki_data:response.data.wikipedia
-        };
+            response_data = {
+              weather_current:response.data.weather.current,
+              five_day:response.data.weather.five_day,
+              location_info:response.data.location_info,
+              wiki_data:response.data.wikipedia
+            };
 
-        // render the response page with the data
-        res.render("pages/results", response_data);
-
+            // render the response page with the data
+            res.render("pages/results", response_data);
+          })
+          .catch(()=>{
+            // error getting the wikipedia data or api data.
+            res.render("pages/index", {message:"Error getting location data, please try again."});
+          });
+        }
       })
-      .catch(()=>{
-        // error getting the wikipedia data or api data.
-        res.render("pages/index", {message:"Error getting location data, please try again."});
+      .catch((err)=>{
+        // Error in validating the zip code, server side problem.
+        res.render("pages/index", {message:"Error validating zip, please try try again."});
       });
-
-    })
-    .catch((err)=>{
-      // Error in validating the zip code, server side problem.
-      res.render("pages/index", {message:"Error validating zip, please try try again."});
-    });
     }
-
 });
+
 
 
 app.get("/results", (req, res)=>{
   res.redirect("/");
 });
+
 
 
 app.listen(PORT, ()=>{
